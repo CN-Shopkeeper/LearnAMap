7fb4d8c6de3ded1c0358dac792e382f189d6a1fa
shopkeeper <734212732@qq.com>
2021-10-01 09:35:26 +0800

    feature(DisplayMapActivity): add DiaplayMapActivity, now can show the basic map

--- app/src/main/java/com/shopkeeper/learnamap/createMap/maps/DisplayMapActivity.java
+++ app/src/main/java/com/shopkeeper/learnamap/createMap/maps/DisplayMapActivity.java
@@ -2,15 +2,29 @@ package com.shopkeeper.learnamap.createMap.maps;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.shopkeeper.learnamap.R;
+import com.shopkeeper.learnamap.databinding.ActivityDisplayMapBinding;
 
 public class DisplayMapActivity extends AppCompatActivity {
+
+    private ActivityDisplayMapBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_map);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_display_map);
+        binding.mapView.onCreate(savedInstanceState);
+//        AMap aMap = null;
+//        aMap = binding.mapView.getMap();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        binding.mapView.onSaveInstanceState(outState);
     }
 }
