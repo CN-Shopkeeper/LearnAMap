8ef9b8a190aeaea3e61f7399a18a93b370d5287c
shopkeeper <734212732@qq.com>
2021-10-06 16:35:39 +0800

    feature(Draw3DModelActivity): add Draw3DModelActivity

--- app/src/main/java/com/shopkeeper/learnamap/drawOnMap/DrawOnMapActivity.java
+++ app/src/main/java/com/shopkeeper/learnamap/drawOnMap/DrawOnMapActivity.java
@@ -3,6 +3,7 @@ package com.shopkeeper.learnamap.drawOnMap;
 import android.os.Bundle;
 
 import com.shopkeeper.learnamap.RedirectActivity;
+import com.shopkeeper.learnamap.drawOnMap.maps.Draw3DModelActivity;
 import com.shopkeeper.learnamap.drawOnMap.maps.DrawHeatMapActivity;
 import com.shopkeeper.learnamap.drawOnMap.maps.DrawLineActivity;
 import com.shopkeeper.learnamap.drawOnMap.maps.DrawPlaneActivity;
@@ -10,12 +11,13 @@ import com.shopkeeper.learnamap.drawOnMap.maps.DrawPointActivity;
 
 public class DrawOnMapActivity extends RedirectActivity {
 
-    private final String[] names = {"绘制点标记", "绘制线", "绘制平面", "绘制热力图"};
+    private final String[] names = {"绘制点标记", "绘制线", "绘制平面", "绘制热力图", "绘制3D模型"};
     private final Class<?>[] classes = {
             DrawPointActivity.class,
             DrawLineActivity.class,
             DrawPlaneActivity.class,
-            DrawHeatMapActivity.class
+            DrawHeatMapActivity.class,
+            Draw3DModelActivity.class
     };
 
     @Override
